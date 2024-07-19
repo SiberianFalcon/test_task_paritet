05f20437e7fb2cba6fb72d71733a7763e4ce8668
SiberianFalcon <bq.Sokol@yandex.ru>
2024-07-19 10:56:25 +0700

    backend_is_done

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class TestTaskModel(models.Model):
     text = models.TextField()
-    picture = models.ImageField(upload_to='images', null=True, default=None)
+    picture = models.ImageField(upload_to='images')
 
     def delete(self):
         os.remove(self.picture.path)
